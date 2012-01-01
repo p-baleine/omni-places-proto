f8597e94055feeb82ca212619a9d1c55e88b6578
Tajima Junpei <p.baleine@gmail.com>
2012-01-01 14:41:46 +0900

    oauth fb

--- config/boot.rb
+++ config/boot.rb
@@ -7,6 +7,7 @@ Bundler.require(:default, ENV["RACK_ENV"].to_sym)
 
 Dir["./lib/**/*.rb"].each { |f| require f }
 Dir["./app/controllers/**/*.rb"].each { |f| require f }
+Dir["./app/services/**/*.rb"].each { |f| require f }
 
 FileUtils.mkdir_p 'log' unless File.exists?('log')
 log = File.new("log/sinatra.log", "a")
