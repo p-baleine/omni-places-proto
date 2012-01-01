9c06154b5e4a9fce1aaaa335e15db731e2b2958a
Tajima Junpei <p.baleine@gmail.com>
2012-01-01 10:13:23 +0900

    add oauth proc

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -9,10 +9,10 @@ SiteConfig = OpenStruct.new(
 
 module Example
   class Web < Sinatra::Base
-    use Rack::Session::Cookie
-    use OmniAuth::Builder do
-      provider :twitter, 'etqFT6toO5bFOi03usnCg', 'Brxl7bMlXZ8GJa87wm5E2VU34rhZuNJENhDkBypY18'
-    end
+    # use Rack::Session::Cookie
+    # use OmniAuth::Builder do
+    #   provider :twitter, 'etqFT6toO5bFOi03usnCg', 'Brxl7bMlXZ8GJa87wm5E2VU34rhZuNJENhDkBypY18'
+    # end
 
     configure do
       set :public_folder, "#{File.dirname(__FILE__)}/../public"
@@ -24,10 +24,10 @@ module Example
       haml :index
     end
 
-    post '/auth/:name/callback' do
-      auth = request.env['omniauth.auth']
-      p auth
-    end
+    # post '/auth/:name/callback' do
+    #   auth = request.env['omniauth.auth']
+    #   p auth
+    # end
 
   end
 end
